f1cd5918716b67b6a6e7f1c483ae38f8beda438d
weblogger117-art <weblogger117@gmail.com>
2026-07-20 14:51:19 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import WhyOctoWare from "@/components/sections/WhyOctoWare";
 import Statistics from "@/components/sections/Statistics";
 import Platform from "@/components/sections/Platform";
 import OctoWareNET from "@/components/sections/OctoWareNET";
+import OctoWareTN from "@/components/sections/OctoWareTN";
 
 export default function HomePage() {
   return (
@@ -17,6 +18,7 @@ export default function HomePage() {
         <WhyOctoWare />
          <Platform />
         <OctoWareNET />
+        <OctoWareTN />
       </main>
 
       <Footer />
