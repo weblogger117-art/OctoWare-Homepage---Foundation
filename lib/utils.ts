dfe03c3e448278e8f99dac1e53a5bdbfc3a848ae
weblogger117-art <weblogger117@gmail.com>
2026-07-20 13:57:34 +0200

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,14 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-/**
- * Führt beliebige Tailwind-Klassen intelligent zusammen.
- *
- * Beispiel:
- *
- * cn("px-4", "px-8")
- * => "px-8"
- */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
