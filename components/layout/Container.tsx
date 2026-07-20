828074f9308f681f58d809908cd313f703740147
weblogger117-art <weblogger117@gmail.com>
2026-07-20 13:33:27 +0200

    Create Container.tsx

---
+++ components/layout/Container.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+type ContainerProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+export default function Container({
+  children,
+  className = ""
+}: ContainerProps) {
+  return (
+    <div className={`container ${className}`.trim()}>
+      {children}
+    </div>
+  );
+}
