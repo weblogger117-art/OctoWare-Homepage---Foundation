18729fcc41fb98c8294af74875519f5ffdd78d01
weblogger117-art <weblogger117@gmail.com>
2026-07-20 14:04:50 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 import Hero from "@/components/sections/Hero";
 import WhyOctoWare from "@/components/sections/WhyOctoWare";
+import Statistics from "@/components/sections/Statistics";
 
 export default function HomePage() {
   return (
@@ -10,6 +11,7 @@ export default function HomePage() {
 
       <main>
         <Hero />
+        <Statistics />
         <WhyOctoWare />
       </main>
 
